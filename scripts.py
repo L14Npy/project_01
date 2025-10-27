dc572cc8e04143e21faad32eda92badc6037ab92
Lian <228531810+L14Npy@users.noreply.github.com>
2025-10-27 09:14:08 -0400

    working

--- scripts.py
+++ scripts.py
@@ -1,11 +1,37 @@
 import json
 
 """ --------------------------------------------------------------------- """
-
-def count_mipymes():
-    with open('./datasets/mipyme_dataset.json', 'r', encoding='utf-8') as f:
+# Contador de MiPymes & Productos
+def mipymes_products_count():
+    with open('./datasets/mipymes.json', 'r', encoding='utf-8') as f:
         data = json.load(f)
 
-    print(f'Número de MiPymes: {len(data["mipymes"])}')
+    mipymes = data["mipymes"]
+    n_products = []
 
+    for i in range(len(mipymes)):
+        products = mipymes[i]["products"]
+        n_products.append(len(products))
+
+    print(f'Número de MiPymes: {len(mipymes)}')
+    print(f'Cantidad total de productos: {sum(n_products)}')
+
+""" --------------------------------------------------------------------- """
+# Nombre de MiPyme, Dirección & Cantidad de Productos
+def mipymes_town():
+    with open('./datasets/mipymes.json', 'r', encoding='utf-8') as f:
+        data = json.load(f)
+
+    mipymes = data["mipymes"]
+
+    for i in range(len(mipymes)):
+        name = mipymes[i]["place"]["name"]
+        street = mipymes[i]["place"]["location"]["street"]
+        products = mipymes[i]["products"]
+
+        print(f'MiPyme: {name}.\n - Dirección: {street}.\n - Cantidad de productos: {len(products)}')
+
+""" --------------------------------------------------------------------- """
+""" --------------------------------------------------------------------- """
+""" --------------------------------------------------------------------- """
 """ --------------------------------------------------------------------- """
