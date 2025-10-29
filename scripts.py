422096114deef3a952e7baa56810ab91da5f5fd6
Lian <228531810+L14Npy@users.noreply.github.com>
2025-10-29 10:54:04 -0400

    function

--- scripts.py
+++ scripts.py
@@ -32,6 +32,47 @@ def mipymes_town():
         print(f'MiPyme: {name}.\n - Dirección: {street}.\n - Cantidad de productos: {len(products)}')
 
 """ --------------------------------------------------------------------- """
+# Promedio Total
+def average_total():
+    with open('./datasets/mipymes.json', 'r', encoding='utf-8') as f:
+        data = json.load(f)
+
+    mipymes = data["mipymes"]
+    n_prices = []
+
+    for i in range(len(mipymes)):
+        products = mipymes[i]["products"]
+
+        for j in range(len(products)):
+            records = products[j]["records"]
+            
+            for k in range(len(records)):
+                price = records[k]["price"]
+                if price != 0.0:
+                    n_prices.append(price)
+
+    return sum(n_prices) // len(n_prices)
+
+""" --------------------------------------------------------------------- """
+
+def brand():
+    with open('./datasets/mipymes.json', 'r', encoding='utf-8') as f:
+        data = json.load(f)
+
+    mipymes = data["mipymes"]
+    n_brands = []
+
+    for i in range(len(mipymes)):
+        products = mipymes[i]["products"]
+        
+        for j in range(len(products)):
+            brand = products[j]["brand"]
+            if (brand not in n_brands) and (brand != None) and (brand != ""):
+                n_brands.append(brand)
+
+    print(f'Cantidad de Marcas: {len(n_brands)} \n- Marcas: {', '.join(n_brands)}')
+
+""" --------------------------------------------------------------------- """
 """ --------------------------------------------------------------------- """
 """ --------------------------------------------------------------------- """
 """ --------------------------------------------------------------------- """
