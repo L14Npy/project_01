9b63b5b53f75cac49a99c570379919589eb27e10
L14N <228531810+L14Npy@users.noreply.github.com>
2025-12-06 07:48:48 -0500

    return

--- normalize.py
+++ normalize.py
@@ -1,85 +1,75 @@
 import json
-""" -------------------------------------------------------------------- """
-path1 = './datasets/mipymes.json'
-path2 = './datasets/salaries.json'
-""" -------------------------------------------------------------------- """
-def OpenJSON(path):
-    # Leer archivos en formato JSON
+""" ------------------------------------------------------------- """
+def OpenJSON(path, k):
+    # Abre documentos en formato JSON
     with open(path, 'r', encoding='utf-8') as f:
         data = json.load(f)
-
-    return data
-""" -------------------------------------------------------------------- """
-def Dicc(k, v, path=path1):
-    # Agrega a un diccionario claves["Place"] y valores["Products"]
-    # Valores: Categorías o subcategorías sín repetir
-    mipymes = OpenJSON(path).get("mipymes")
+    return data.get(k, [])
+""" ------------------------------------------------------------- """
+mipymes = OpenJSON('./datasets/mipymes.json', 'mipymes')
+salaries = OpenJSON('./datasets/salaries.json', 'salaries')
+""" ------------------------------------------------------------- """
+def Sum(array):
+    # Retorna la suma de una lista
+    sum = 0
+    for i in array:
+        sum += i
+    return sum
+""" ------------------------------------------------------------- """
+def Average():
+    # Retorna el promedio total de una lista
+    array = []
+    for i in salaries:
+        array.append(i["salarie"])
+    return Sum(array) // len(array)
+""" ------------------------------------------------------------- """
+average = Average()
+""" ------------------------------------------------------------- """
+def DiccJSON(k):
+    # Genera un diccionario con claves específicas a partir del archivo JSON
     dicc = {}
-
-    for i in range(len(mipymes)):
-        products = mipymes[i]["products"]
-        key = mipymes[i]["place"][k]
-        
-        if key not in dicc:
-            dicc[key] = set()
-        
-        for j in range(len(products)):
-            value = products[j][v]
-            dicc[key].add(value)
-
-    return dicc.items()
-""" -------------------------------------------------------------------- """
-def Dicc2(k, v, path=path1):
-    # Agrega a un diccionario claves["Products"] y valores["Products"]
-    # Valores: Lista de cantidades de peso neto
-    mipymes = OpenJSON(path).get("mipymes")
-    dicc = {}
-
-    for i in range(len(mipymes)):
-        products = mipymes[i].get("products")
-
-        for j in range(len(products)):
-            key = products[j][k]
-            value = products[j][v]
-
-            if value is None:
-                continue
+    for i in mipymes:
+        for j in i["products"]:
+            key = j[k]
             if key not in dicc:
                 dicc[key] = []
-            dicc[key].append(value)
-
-    return dicc.items()
-""" -------------------------------------------------------------------- """
-def Dicc3(k, path=path1):
-    # Agrega a un diccionario
-    mipymes = OpenJSON(path).get("mipymes")
+            dicc[key].append(j["count"])
+    return dicc
+""" ------------------------------------------------------------- """
+def Dicc(k):
+    # Genera un diccionario con el valor moda
     dicc = {}
-
-    for i in range(len(mipymes)):
-        products = mipymes[i]["products"]
-
-        for j in range(len(products)):
-            key = products[j][k]
-
-            if key == "" or key is None:
-                continue
-            dicc[key] = dicc.get(key, 0) + 1
-
+    for k,v in DiccJSON(k).items():
+        val = Moda(v)
+        dicc[k] = val
     return dicc
-""" -------------------------------------------------------------------- """
-def OrderDicc(k, n):
-    # Ordena el diccionario por valores (enteros)
-    items = [i for i in Dicc3(k).items()]
-    for i in range(len(items)):
+""" ------------------------------------------------------------- """
+def DiccOrder(k, n):
+    # Organiza el diccionario en orden alfabético
+    array = [i for i in Dicc(k).items()]
+    for i in range(len(array)):
         min = i
-        for j in range(i+1, len(items)):
-            if items[j][1] > items[min][1]:
+        for j in range(i+1, len(array)):
+            if array[j][0] < array[i][0]:
                 min = j
-        items[i], items[min] = items[min], items[i]
-
-    dicc = {k:v for k,v in items[:n]}
-    return dicc
-""" -------------------------------------------------------------------- """
-""" -------------------------------------------------------------------- """
-""" -------------------------------------------------------------------- """
-""" -------------------------------------------------------------------- """
+        array[i], array[min] = array[min], array[i]
+    return {k:v for k,v in array[:n]}
+""" ------------------------------------------------------------- """
+def Moda(array):
+    # Retorna el elemento que más se repite
+    repeat = 0
+    moda = None
+    for i in range(len(array)):
+        counter = 0
+        for j in range(len(array)):
+            if array[j] == array[i]:
+                counter += 1
+        if counter > repeat:
+            repeat = counter
+            moda = array[i]
+    return moda
+""" ------------------------------------------------------------- """
+""" ------------------------------------------------------------- """
+""" ------------------------------------------------------------- """
+""" ------------------------------------------------------------- """
+""" ------------------------------------------------------------- """
